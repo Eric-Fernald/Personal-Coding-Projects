dc5dd58b1e9d2d99e10dd70cd4d89bda682f280b
Eric Fernald <ejf4595@gmail.com>
2020-12-27 11:03:09 -0500

    Added Unquote Android Game.

---
+++ Projects/Unquote Android Game/app/src/main/java/com/codecademy/unquote/MainActivity.java
@@ -0,0 +1,126 @@
+package com.codecademy.unquote;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+    ArrayList<Question> questions;
+
+    // TODO 3-A: Declare View member variables
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO 2-G: Show app icon in ActionBar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_unquote_icon);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setElevation(0);
+        setContentView(R.layout.activity_main);
+
+        // TODO 3-B: assign View member variables
+
+        // TODO 4-E: set onClickListener for each answer Button
+
+        // TODO 5-A: set onClickListener for the submit answer Button
+
+        startNewGame();
+    }
+
+    // TODO 3-F: displayQuestion(Question question) {...}
+
+    // TODO 3-C: displayQuestionsRemaining(int questionRemaining) {...}
+
+    // TODO 4-A: onAnswerSelected(int answerSelected) {...}
+
+    void onAnswerSubmission() {
+        Question currentQuestion = getCurrentQuestion();
+        if (currentQuestion.isCorrect()) {
+            totalCorrect = totalCorrect + 1;
+        }
+        questions.remove(currentQuestion);
+
+        // TODO 3-D.i: Uncomment the line below after implementing displayQuestionsRemaining(int)
+        // displayQuestionsRemaining(questions.size());
+
+        if (questions.size() == 0) {
+            String gameOverMessage = getGameOverMessage(totalCorrect, totalQuestions);
+
+            // TODO 5-D: Show a popup instead
+            System.out.println(gameOverMessage);
+        } else {
+            chooseNewQuestion();
+
+            // TODO 3-H.i: uncomment after implementing displayQuestion(Question)
+            // displayQuestion(getCurrentQuestion());
+        }
+    }
+
+    void startNewGame() {
+        questions = new ArrayList<>();
+
+        // TODO 2-H: Provide actual drawables for each of these questions!
+        Question question0 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question1 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question2 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question3 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question4 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+        Question question5 = new Question(0, "This is a test question!", "Test Answer 0", "Test Answer 1", "Test Answer 2", "Test Answer 3", 2);
+
+        questions.add(question0);
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
+
+        totalCorrect = 0;
+        totalQuestions = questions.size();
+
+        Question firstQuestion = chooseNewQuestion();
+
+        // TODO 3-D.ii: Uncomment the line below after implementing displayQuestionsRemaining(int)
+        // displayQuestionsRemaining(questions.size());
+
+        // TODO 3-H.ii: Uncomment after implementing displayQuestion(Question)
+        // displayQuestion(firstQuestion);
+    }
+
+    Question chooseNewQuestion() {
+        int newQuestionIndex = generateRandomNumber(questions.size());
+        currentQuestionIndex = newQuestionIndex;
+        return questions.get(currentQuestionIndex);
+    }
+
+    int generateRandomNumber(int max) {
+        double randomNumber = Math.random();
+        double result = max * randomNumber;
+        return (int) result;
+    }
+
+    Question getCurrentQuestion() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        return currentQuestion;
+    }
+
+    String getGameOverMessage(int totalCorrect, int totalQuestions) {
+        if (totalCorrect == totalQuestions) {
+            return "You got all " + totalQuestions + " right! You won!";
+        } else {
+            return "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
+        }
+    }
+}
