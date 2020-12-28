2cf2b66f8f9544545273a274f009e51f78fa7c7c
Eric Fernald <ejf4595@gmail.com>
2020-12-28 13:24:35 -0500

    Added comments to Merge Sort.

--- Projects/Python Scripts/Python Merge Sort.py
+++ Projects/Python Scripts/Python Merge Sort.py
@@ -1,6 +1,7 @@
 ##########
 # This script does a merge sort of an unordered list and prints an ordered list.
 ##########
+#Define the merge sort function.
 def merge_sort(items):
   if len(items) <= 1:
     return items
@@ -13,7 +14,7 @@ def merge_sort(items):
   right_sorted = merge_sort(right_split)
 
   return merge(left_sorted, right_sorted)
-
+#After splitting the items into a left and right list, now the merge is defined.
 def merge(left, right):
   result = []
 
@@ -29,9 +30,9 @@ def merge(left, right):
     result += left
   if right:
     result += right
-
+#Returns the merge.
   return result
-
+#Sample lists to test against.
 unordered_list1 = [356, 746, 264, 569, 949, 895, 125, 455]
 unordered_list2 = [787, 677, 391, 318, 543, 717, 180, 113, 795, 19, 202, 534, 201, 370, 276, 975, 403, 624, 770, 595, 571, 268, 373]
 unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
@@ -39,7 +40,7 @@ unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
 ordered_list1 = merge_sort(unordered_list1)
 ordered_list2 = merge_sort(unordered_list2)
 ordered_list3 = merge_sort(unordered_list3)
-
+#Print lists to terminal.
 print(ordered_list1)
 print(ordered_list2)
 print(ordered_list3)
