c5f2ea389db1723baa27ed35c9adf7310dc7f4ef
Eric Fernald <ejf4595@gmail.com>
2020-12-28 21:22:05 -0500

    Added the linked list function, the node, and the main script.

---
+++ Projects/Python Scripts/Sorting A Linked List/script.py
@@ -0,0 +1,51 @@
+from linkedlist import LinkedList
+
+def find_max(linked_list):
+  current = linked_list.get_head_node()
+  maximum = current.get_value()
+  while current.get_next_node():
+    current = current.get_next_node()
+    val = current.get_value()
+    if val > maximum:
+      maximum = val
+  return maximum
+
+#Fill in Function
+def sort_linked_list(linked_list):
+  print("\n---------------------------")
+  print("The original linked list is:\n{0}".format(linked_list.stringify_list()))
+  new_linked_list = LinkedList()
+  
+  while linked_list.get_head_node():
+    current_max = find_max(linked_list)
+    linked_list.remove_node(current_max)
+    new_linked_list.insert_beginning(current_max)
+  
+  return new_linked_list
+
+#Test Cases
+ll = LinkedList("Z")
+ll.insert_beginning("C")
+ll.insert_beginning("Q")
+ll.insert_beginning("A")
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll).stringify_list()))
+
+ll_2 = LinkedList(1)
+ll_2.insert_beginning(4)
+ll_2.insert_beginning(18)
+ll_2.insert_beginning(2)
+ll_2.insert_beginning(3)
+ll_2.insert_beginning(7)
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_2).stringify_list()))
+
+ll_3 = LinkedList(-11)
+ll_3.insert_beginning(44)
+ll_3.insert_beginning(118)
+ll_3.insert_beginning(1000)
+ll_3.insert_beginning(23)
+ll_3.insert_beginning(-92)
+print("The sorted linked list is:\n{0}".format(sort_linked_list(ll_3).stringify_list()))
+
+#Runtime
+runtime = "N^2"
+print("The runtime of sort_linked_list is O({0})\n\n".format(runtime))
