904dc611fccf0698def265feb2ef24deb0e8d7d1
Eric Fernald <ejf4595@gmail.com>
2020-12-30 17:17:45 -0500

    Added an extra space for the note.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -4,5 +4,6 @@ import time
 connection = krpc.connect()
 
 vessel = connection.space_center.active_vessel
+
 #Note: Set the staging is set properly so that the parachute doesn't deploy at the same time as the booster.
 vessel.control.activate_next_stage()
