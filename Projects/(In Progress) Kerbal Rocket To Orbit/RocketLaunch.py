cfc7cfc0c9f3df96b19221c1706c7be307ff17f7
Eric Fernald <ejf4595@gmail.com>
2020-12-30 17:17:17 -0500

    Added the main vessel control system and stage activation system.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -1,3 +1,8 @@
 import krpc
 import time 
 
+connection = krpc.connect()
+
+vessel = connection.space_center.active_vessel
+#Note: Set the staging is set properly so that the parachute doesn't deploy at the same time as the booster.
+vessel.control.activate_next_stage()
