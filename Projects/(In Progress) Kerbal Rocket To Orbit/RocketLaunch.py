8093b0ab74ee21e67a1f548abfec0eaffb8686f1
Eric Fernald <ejf4595@gmail.com>
2021-01-02 13:41:35 -0500

    Added countdown sequence.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -11,3 +11,11 @@ vessel = connection.space_center.active_vessel
 
 #Note: Set the staging is set properly so that the parachute doesn't deploy at the same time as the booster.
 vessel.control.activate_next_stage()
+
+#Countdown sequence.
+countdown = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One", "LIFT OFF!"]
+
+for i in range(len(countdown)):
+    print(countdown[i])
+    time.sleep(1)
+    
