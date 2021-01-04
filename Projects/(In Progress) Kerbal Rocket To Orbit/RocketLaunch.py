16a48704e7c408ff12f59f600c62fab986f1dc24
Eric Fernald <ejf4595@gmail.com>
2021-01-04 16:16:23 -0500

    Defines the ascent phase and cruise phase once the vessel reaches orbit.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -47,6 +47,9 @@ while ascentPhase or cruisePhase or insertionPhase:
             vessel.control.sas = True  
             time.sleep(0.1)
             vessel.control.sas_mode = conn.space_center.SASMode.prograde
+
+            ascentPhase = False
+            cruisePhase = True
     
     elif cruisePhase:
         print("Cruise Phase")
