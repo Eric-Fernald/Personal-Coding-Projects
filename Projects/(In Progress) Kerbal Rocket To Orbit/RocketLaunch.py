c4669a058aa89b8d1c32f1f04f6b1c7cd7a748ae
Eric Fernald <ejf4595@gmail.com>
2021-01-04 16:21:29 -0500

    Added comments describing each block of code.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -12,9 +12,10 @@ vessel = connection.space_center.active_vessel
 #Note: Set the staging is set properly so that the parachute doesn't deploy at the same time as the booster.
 vessel.control.activate_next_stage()
 
-#Countdown sequence.
+#Countdown Sequence.
 countdown = ["Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One", "LIFT OFF!"]
 
+#Start the Countdown Sequence.
 for i in range(len(countdown)):
     print(countdown[i])
     time.sleep(1)
@@ -22,6 +23,7 @@ for i in range(len(countdown)):
 vessel.control.throttle = 1
 vessel.control.activate_next_stage()
 
+#Flight State
 ascentPhase = True
 cruisePhase = False
 insertionPhase = False
@@ -34,6 +36,7 @@ while ascentPhase or cruisePhase or insertionPhase:
         targetPitch = 90 * ((50000 - altitude) / 50000))
         pitchDiff = vessel.flight().pitch - targetPitch
 
+        #Heading Control
         if heading < 180:
             vessel.control.yaw = (pitchDiff / 90)
         else:
@@ -43,6 +46,7 @@ while ascentPhase or cruisePhase or insertionPhase:
         if vessel.thrust == 0.0:
             vessel.control.activate_next_stage()
         
+        #MECO (Main Engine Cut Off)
         if vessel.orbit.apoapsis > 700000:
             vessel.control.throttle = 0
             time.sleep(0.5)
