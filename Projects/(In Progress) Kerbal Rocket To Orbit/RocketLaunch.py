32c6af991055799929275270a8500dbde88b33a7
Eric Fernald <ejf4595@gmail.com>
2021-01-04 15:14:51 -0500

    Defines the target pitch for the rocket in the Ascent Phase.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -31,6 +31,7 @@ while ascentPhase or cruisePhase or insertionPhase:
     heading = vessel.flight().heading
 
     if ascentPhase:
+        targetPitch = (50000 - altitude) / 50000
     
     elif cruisePhase:
     
