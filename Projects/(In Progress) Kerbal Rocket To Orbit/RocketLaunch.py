8faa731ab07e66c0f61108dff8f8a92dd0ae5268
Eric Fernald <ejf4595@gmail.com>
2021-01-04 21:04:34 -0500

    Edited connection function.

--- Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
+++ Projects/(In Progress) Kerbal Rocket To Orbit/RocketLaunch.py
@@ -17,9 +17,9 @@ import krpc
 import time 
 
 #Establishes connection to the krpc server.
-connection = krpc.connect()
+conn = krpc.connect()
 
-vessel = connection.space_center.active_vessel
+vessel = conn.space_center.active_vessel
 
 #Note: Set the staging is set properly so that the parachute doesn't deploy at the same time as the booster.
 vessel.control.activate_next_stage()
