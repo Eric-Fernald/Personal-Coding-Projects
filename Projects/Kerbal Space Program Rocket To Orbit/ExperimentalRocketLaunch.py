de33c9ec60bded053fe3327eb77fc7d1fd865d81
Eric Fernald <ejf4595@gmail.com>
2021-01-07 14:13:41 -0500

    Added the definition for the panel.

--- Projects/Kerbal Space Program Rocket To Orbit/ExperimentalRocketLaunch.py
+++ Projects/Kerbal Space Program Rocket To Orbit/ExperimentalRocketLaunch.py
@@ -26,6 +26,9 @@ canvas = conn.ui.stock_canvas
 # Get the size of the game window in pixels.
 screen_size = canvas.rect_transform.size
 
+#Defines the panel to hold UI elements.
+panel = canvas.add_panel()
+
 # Position the panel on the left of the screen.
 rect = panel.rect_transform
 rect.size = (400, 100)
