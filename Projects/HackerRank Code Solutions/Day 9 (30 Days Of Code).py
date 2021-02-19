9e3841108efbcf82f5dc370849cdd45ff1200c24
Eric Fernald <ejf4595@gmail.com>
2021-02-19 09:36:46 -0500

    Added Day 9 of 30 Days of Code.

---
+++ Projects/HackerRank Code Solutions/Day 9 (30 Days Of Code).py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n<=1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+n = int(input())
+print(factorial(n))
