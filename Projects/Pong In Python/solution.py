d0bc556b3223c236249d0fbcae72ca6037bf5c14
Eric Fernald <ejf4595@gmail.com>
2022-02-16 06:40:30 -0500

    Added clock.

--- Projects/Pong In Python/solution.py
+++ Projects/Pong In Python/solution.py
@@ -8,13 +8,17 @@ pygame.display.set_caption("PONG")
 
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
