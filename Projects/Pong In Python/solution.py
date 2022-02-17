c5ae81d3fed962c0c495c6ee83ae20a3d4b072d9
Eric Fernald <ejf4595@gmail.com>
2022-02-16 20:57:43 -0500

    Created a paddle class.

--- Projects/Pong In Python/solution.py
+++ Projects/Pong In Python/solution.py
@@ -10,8 +10,20 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rectangle(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
 def draw(win):
-    win.fill(WHITE)
+    win.fill(BLACK)
     pygame.display.update()
 
 
