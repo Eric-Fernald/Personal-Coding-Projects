5348355049f02771ea0493dd48dd6632267901f4
Eric Fernald <ejf4595@gmail.com>
2022-02-26 16:33:01 -0500

    Added screen limits for paddles.

--- Projects/Pong In Python/solution.py
+++ Projects/Pong In Python/solution.py
@@ -1,7 +1,6 @@
 import pygame
 pygame.init()
 
-
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PONG")
@@ -40,14 +39,14 @@ def move(self, up=True):
         self.y += self.VEL
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
         left_paddle.move(up=True)
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
         left_paddle.move(up=False)
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
         right_paddle.move(up=True)
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
         right_paddle.move(up=False)
 
 
