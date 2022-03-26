f4a4e629fa706a406a5b7346d5d27591a2ff5a25
Eric Fernald <ejf4595@gmail.com>
2022-03-26 10:03:44 -0400

    Added Valid Phone Numbers and Contains Duplicate.

---
+++ Projects/Leetcode Code Solutions/193. Valid Phone Numbers. Valid Phone Numbers.sh
@@ -0,0 +1,8 @@
+# Read from the file file.txt and output all valid phone numbers to stdout.
+#!/usr/bin/env bash
+
+while read line; do
+    if [[ "$line" =~ ^((\([0-9]{3}\) )|[0-9]{3}-)[0-9]{3}-[0-9]{4}$ ]]; then
+        echo $line
+    fi
+done < "file.txt"
