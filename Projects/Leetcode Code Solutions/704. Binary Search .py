855879f81a61d172e0317769af6594e11ed6220a
Eric Fernald <ejf4595@gmail.com>
2022-03-26 10:08:23 -0400

    Added solution for Binary Search.

---
+++ Projects/Leetcode Code Solutions/704. Binary Search .py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
