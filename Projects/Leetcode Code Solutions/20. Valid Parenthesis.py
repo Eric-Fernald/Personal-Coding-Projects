7fec7d8d17c518193e658fb533cfa1bbe7eaf85f
Eric Fernald <ejf4595@gmail.com>
2022-03-26 09:59:16 -0400

    Added solution for Valid Parenthesis.

---
+++ Projects/Leetcode Code Solutions/20. Valid Parenthesis.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        for c in s:
+            if c in ['(', '[', '{']:
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                if c == ')' and stack[-1] != '(':
+                    return False
+                if c == ']' and stack[-1] != '[':
+                    return False
+                if c == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+        return len(stack) == 0
