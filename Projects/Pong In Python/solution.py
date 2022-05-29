642b6159a5cc5fd7d22d26657f8c8291239b4d4e
Eric Fernald <ejf4595@gmail.com>
2022-05-29 11:16:17 -0400

    Added score font and winning score needed.

--- Projects/Pong In Python/solution.py
+++ Projects/Pong In Python/solution.py
@@ -11,6 +11,8 @@ BLACK = (0, 0, 0)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_RADIUS = 7
+SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+WINNING_SCORE = 10
 class Paddle:
     COLOR = WHITE
     VEL = 4
