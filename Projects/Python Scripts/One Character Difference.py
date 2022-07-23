50a965cee6bbc9522de6678cc8860cb6041aee03
Eric Fernald <ejf4595@gmail.com>
2022-07-22 20:00:11 -0400

    Added One Character Difference Function.

---
+++ Projects/Python Scripts/One Character Difference.py
@@ -0,0 +1,14 @@
+# write a function that compares two strings and returns true if there is only one character difference between them
+
+def one_diff(a, b):
+    if len(a) != len(b):
+        return False
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            if a[i] == b[i + 1] or a[i + 1] == b[i]:
+                return True
+            else:
+                return False
+    return False
+
+one_diff("test", "text")
