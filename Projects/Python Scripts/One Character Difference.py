8ac4e615f50cf34e83c2e09bf22e9e29cc6329da
Eric Fernald <ejf4595@gmail.com>
2022-07-22 20:15:46 -0400

    Changed same characters to different characters.

--- Projects/Python Scripts/One Character Difference.py
+++ Projects/Python Scripts/One Character Difference.py
@@ -3,11 +3,10 @@ def compare_strings(a, b):
         print("Number of Same Characters: 0")
         return
     size = min(len(a), len(b))
-    count = 0 
+    count = 0
     for i in range(size):
-        if a[i] == b[i]:
+        if a[i] != b[i]:
             count += 1
 
-    print("Number of Same Characters:", count)
-
-print(compare_strings("test", "text"))
+    print("Number of Different Characters:", count)
+compare_strings("abcd", "abdc")
