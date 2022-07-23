6dd37d9b721e9595f12dff7326fa31ee5befe6c7
Eric Fernald <ejf4595@gmail.com>
2022-07-22 20:19:00 -0400

    Changed return to print statements for testing.

--- Projects/Python Scripts/One Character Difference.py
+++ Projects/Python Scripts/One Character Difference.py
@@ -1,14 +1,14 @@
 def compare_strings(a, b):
     if a is None or b is None:
-        return False
+        print("False")
     size = min(len(a), len(b))
     count = 0
     for i in range(size):
         if a[i] != b[i]:
             count += 1
         if count <= 1:
-            return True
+            print("True")
         else:
-            return False
+            print("False")
             
 compare_strings("abcd", "abdc")
