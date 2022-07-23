aa87222acc293645d3293bb4162170a64a67ae09
Eric Fernald <ejf4595@gmail.com>
2022-07-22 20:30:41 -0400

    Fixed comparison operator.

--- Projects/Python Scripts/One Character Difference.py
+++ Projects/Python Scripts/One Character Difference.py
@@ -6,7 +6,7 @@ def compare_strings(a, b):
     for i in range(size):
         if a[i] != b[i]:
             count += 1
-    if count <= 1:
+    if count == 1:
         print("True")
     else:
         print("False")
