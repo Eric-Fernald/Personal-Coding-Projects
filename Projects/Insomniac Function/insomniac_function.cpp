f932e8c21b17b896d3247679045fc6704d5dba14
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 18:18:47 -0400

    Uncommented function signature.

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -19,13 +19,13 @@ struct TrajectoryResult
   double m_Time;
   bool  m_ValidHit;
 };
-
+*/
 TrajectoryResult PredictTrajectory( const Vec3& start_position, 
                                     const Vec3& start_velocity, 
                                     const Vec3& up_vector, 
                                     double gravity_accel, 
                                     double raycast_time_step, 
-                                    double max_time ); */
+                                    double max_time );
 void TrajectoryResult input(){
     Vec3 start_position = {0,0,0};
     Vec3 start_velocity = {0,0,0};
@@ -34,13 +34,8 @@ void TrajectoryResult input(){
     double raycast_time_step = 0;
     double max_time = 0;
     TrajectoryResult result = PredictTrajectory(start_position, start_velocity, up_vector, gravity_accel, raycast_time_step, max_time);
-    std::cout << result.m_EndPoint.x << std::endl;
-    std::cout << result.m_EndPoint.y << std::endl;
-    std::cout << result.m_EndPoint.z << std::endl;
-    std::cout << result.m_Time << std::endl;
-    std::cout << result.m_ValidHit << std::endl;
-}
 
+}
 int main() {
   TrajectoryResult t;
   t = input();
