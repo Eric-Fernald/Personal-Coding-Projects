658b1c4b85ee676835ba204b5624640942c61a94
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 17:48:16 -0400

    Added struct

---
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -0,0 +1,9 @@
+/*For this test, you will be implementing a function that simulates a projectile trajectory based on its initial starting position and velocity.
+
+The function should return a TrajectoryResult struct, which has the following definition:
+*/
+struct Vec3
+{
+  double x, y, z;
+
+};
