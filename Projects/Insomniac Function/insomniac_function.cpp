be9795d9fc9099b1422b5e8937030a4a1bca640a
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 19:15:26 -0400

    Fixed return name

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -57,9 +57,9 @@ void TrajectoryResult PredictTrajectory(const Vec3& start_position,
         m_ValidHit = false;
     }
     
-    result = {m_EndPoint, m_time, m_ValidHit};
+    TrajectoryResult = {m_EndPoint, m_time, m_ValidHit};
     //Return the result struct
-    return result;
+    return TrajectoryResult;
 
 }
 
