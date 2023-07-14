ad3efa3e6857a30c34000938411fadac8b10f05f
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 17:51:48 -0400

    Added TrajectoryResult.

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -7,3 +7,17 @@ struct Vec3
   double x, y, z;
 
 };
+
+struct TrajectoryResult
+{
+  Vec3 m_EndPoint;
+  double m_Time;
+  bool  m_ValidHit;
+};
+
+TrajectoryResult PredictTrajectory( const Vec3& start_position, 
+                                    const Vec3& start_velocity, 
+                                    const Vec3& up_vector, 
+                                    double gravity_accel, 
+                                    double raycast_time_step, 
+                                    double max_time );
