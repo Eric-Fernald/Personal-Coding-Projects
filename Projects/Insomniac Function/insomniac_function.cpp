7ddc460fe0863e73fbce6e868c79024af3d50692
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 22:13:42 -0400

    Moved struct back within the function

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -14,19 +14,18 @@ Should a default value be set for the endpoint and time if the hit is not valid?
 Is there a desired variable name for the struct?
 */
 
-//Initialize struct
-struct TrajectoryResult {
-      Vec3 m_EndPoint; 
-      double m_time; 
-      bool m_ValidHit;
-    }TResult;
-
 void TrajectoryResult PredictTrajectory(const Vec3& start_position, 
                                         const Vec3& start_velocity, 
                                         const Vec3& up_vector, 
                                         double gravity_accel, 
                                         double raycast_time_step, 
                                         double max_time){
+    struct TrajectoryResult {
+      Vec3 m_EndPoint; 
+      double m_time; 
+      bool m_ValidHit;
+    }TResult;
+
     //Initialize the variables
     m_EndPoint = TResult.m_EndPoint;
     m_time = TResult.m_time;
