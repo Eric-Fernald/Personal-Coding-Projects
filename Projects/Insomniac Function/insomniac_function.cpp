5ab06ec08101bf5dfcec35e260ecd96839683d2d
Eric-Fernald <ejf4595@gmail.com>
2023-07-14 20:31:54 -0400

    Updated questions

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -15,6 +15,7 @@ Questions to ask:
 Will the input always be valid or will I have to check for a valid input?
 Should default values be set for the parameters or the return struct if at all?
 Should a default value be set for the endpoint and time if the hit is not valid?
+Is there a desired variable name for the struct?
 struct Vec3
 {
   double x, y, z;
