9bbc47de5e07813db8d70dac417473522ec95d54
Eric-Fernald <ejf4595@gmail.com>
2023-07-16 10:16:17 -0400

    Added parenthesis for kinetic equation 4.

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -36,7 +36,7 @@ void TrajectoryResult PredictTrajectory( const Vec3& start_position,
     m_ValidHit = TResult.m_ValidHit;
 
     //Calculate the end point of the trajectory
-    m_EndPoint = start_position + start_velocity * m_time + 0.5 * gravity_accel * m_time * m_time;
+    m_EndPoint = start_position + start_velocity * m_time + (0.5 * (gravity_accel * (m_time * m_time)));
 
 
 
