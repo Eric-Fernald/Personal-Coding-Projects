8c993cf0efa38600be914c1a2a92c79837361f31
Eric-Fernald <ejf4595@gmail.com>
2023-07-16 02:06:59 -0400

    Added more variable types

--- Projects/Insomniac Function/insomniac_function.cpp
+++ Projects/Insomniac Function/insomniac_function.cpp
@@ -36,16 +36,16 @@ void TrajectoryResult PredictTrajectory( const Vec3& start_position,
     bool m_ValidHit = TResult.m_ValidHit;
 
     //Calculate the end point of the trajectory
-    m_EndPoint = start_position + start_velocity * m_time + 0.5 * gravity_accel * m_time * m_time;
+    Vec3 m_EndPoint = start_position + start_velocity * m_time + 0.5 * gravity_accel * m_time * m_time;
 
 
 
 
     //Round the endpoint to 2 decimal places
-    m_EndPoint = round(m_EndPoint * 100.0) / 100.0;
+    Vec3 m_EndPoint = round(m_EndPoint * 100.0) / 100.0;
 
     //Round time to 3 decimal places
-    m_time = round(m_time * 1000.0) / 1000.0;
+    double m_time = round(m_time * 1000.0) / 1000.0;
 
     //Check if the time is greater than the max time and if so, set the valid hit to false
     if m_time >= max_time{
