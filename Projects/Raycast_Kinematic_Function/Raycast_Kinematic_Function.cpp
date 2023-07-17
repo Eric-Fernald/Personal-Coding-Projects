8a3bb2175c5e3e82421a73b9a041bf4deb30f5e4
Eric-Fernald <ejf4595@gmail.com>
2023-07-17 07:02:01 -0400

    Updated validhit check

--- Projects/Raycast_Kinematic_Function/Raycast_Kinematic_Function.cpp
+++ Projects/Raycast_Kinematic_Function/Raycast_Kinematic_Function.cpp
@@ -55,7 +55,7 @@ TrajectoryResult PredictTrajectory(const Vec3& start_position,
         current_position.x += current_velocity.x * Sqrtd((2 * current_position.z) / gravity_accel);
         current_position.y += current_velocity.y * raycast_time_step + 0.5 * raycast_time_step * raycast_time_step;
         current_position.z += current_velocity.z * raycast_time_step + 0.5 * raycast_time_step * raycast_time_step;
-        // Might need to change which variables are affected by gravity.
+
         // Calculate the new velocity based on gravity and current time step
         current_velocity.x += up_vector.x * gravity_accel * raycast_time_step;
         current_velocity.y += up_vector.y * gravity_accel * raycast_time_step;
@@ -70,7 +70,7 @@ TrajectoryResult PredictTrajectory(const Vec3& start_position,
         result.m_Time = max_time;
     }
 
-    result.m_ValidHit = false;
+    result.m_ValidHit = valid_hit;
 
     return result;
 }
