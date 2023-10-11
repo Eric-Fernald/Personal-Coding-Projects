69a10d78fb523d2cb129415b1f919a8e9c942182
Eric-Fernald <ejf4595@gmail.com>
2023-10-11 14:18:37 -0400

    Updated OpenSearch Terraform file

--- Projects/Terraform Files/opensearch_cluster.tf
+++ Projects/Terraform Files/opensearch_cluster.tf
@@ -1,13 +1,13 @@
 # Define the AWS provider configuration.
 provider "aws" {
-  region = "us-east-1" # Change to your desired AWS region
+  region = "us-east-2" # Change to your desired AWS region
 }
 
 # Define the OpenSearch provider configuration.
 provider "opensearch" {
-  version = "1.0.0" # Specify the version of the OpenSearch provider
-  endpoint = "your-opensearch-endpoint" # Replace with your OpenSearch endpoint
-  aws_profile = "your-aws-profile" # Replace with your AWS profile if needed
+  required_providers = "1.3.0" # Specify the version of the OpenSearch provider
+  endpoint = "opensearch-v1dot3" # Replace with your OpenSearch endpoint
+  aws_profile = "expressionc2s" # Replace with your AWS profile if needed
 }
 
 # Create a VPC for the OpenSearch cluster.
@@ -17,18 +17,18 @@ resource "aws_vpc" "opensearch_vpc" {
 
 # Create a subnet within the VPC.
 resource "aws_subnet" "opensearch_subnet" {
-  count = 2
+  count = 3
   vpc_id = aws_vpc.opensearch_vpc.id
-  cidr_block = "10.0.1.0/24" # Replace with your desired CIDR block
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+  cidr_block = "10.10.0.0/19"
+  availability_zone = element(["us-east-2a", "us-east-2b", "us-east-2c"], count.index)
 }
 
 # Create a security group for the OpenSearch cluster.
-resource "aws_security_group" "opensearch_sg" {
-  name_prefix = "opensearch-sg-"
-  vpc_id = aws_vpc.opensearch_vpc.id
+#resource "aws_security_group" "opensearch_sg" {
+  #name_prefix = "opensearch-v1dot3"
+  #vpc_id = aws_vpc.opensearch_vpc.id
 
-  # Define your security group rules here.
+  # Define security group rules here if needed.
   # Example:
   # ingress {
   #   from_port   = 80
@@ -36,18 +36,18 @@ resource "aws_security_group" "opensearch_sg" {
   #   protocol    = "tcp"
   #   cidr_blocks = ["0.0.0.0/0"]
   # }
-}
+#}
 
 # Create an OpenSearch domain.
 resource "opensearch_domain" "example" {
-  domain_name = "example-opensearch"
-  elasticsearch_version = "7.10" # Specify the desired OpenSearch version
+  domain_name = "opensearch-v1dot3"
+  elasticsearch_version = "7.10"
   node_to_node_encryption_options {
     enabled = true
   }
   cluster_config {
-    instance_type = "r5.large.search"
-    instance_count = 2
+    instance_type = "r6g.large.search"
+    instance_count = 3
   }
   vpc_options {
     subnet_ids = aws_subnet.opensearch_subnet[*].id
