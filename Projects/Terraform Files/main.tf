1fb8e43eea1f0c65fcc11f00b57e5870cbb4c984
Eric-Fernald <ejf4595@gmail.com>
2024-09-25 16:52:37 -0400

    Updated main.tf

--- Projects/Terraform Files/main.tf
+++ Projects/Terraform Files/main.tf
@@ -1,3 +1,8 @@
-provider "aws" {
-  region = "us-east-1" # Change to your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
