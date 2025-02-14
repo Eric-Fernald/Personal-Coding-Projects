df52bdc63b9abe8fcf30ffdb408f95ef1f21b66b
Eric-Fernald <ejf4595@gmail.com>
2025-02-14 12:11:12 -0500

    add mongoose export module and import

--- AIM Submission/models/item.js
+++ AIM Submission/models/item.js
@@ -8,3 +8,4 @@ const itemSchema = new mongoose.Schema({
 });
 
 const Item = mongoose.model('Item', itemSchema);
+module.exports = Item;
--- AIM Submission/models/kit.js
+++ AIM Submission/models/kit.js
@@ -1,6 +1,9 @@
+const mongoose = require('mongoose');
+
 const kitSchema = new mongoose.Schema({
     name: { type: String, required: true },
     items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
   });
   
 const Kit = mongoose.model('Kit', kitSchema);
+module.exports = Kit;
