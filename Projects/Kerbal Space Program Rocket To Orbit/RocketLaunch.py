c98f44fa79e6e9202d49feedd51f97027d9411fd
Eric-Fernald <ejf4595@gmail.com>
2025-11-06 12:34:59 -0500

    Reverted back to old Rocket Launch code

--- Projects/Kerbal Space Program Rocket To Orbit/RocketLaunch.py
+++ Projects/Kerbal Space Program Rocket To Orbit/RocketLaunch.py
@@ -1,116 +1,102 @@
-import time
+#This program launches a rocket into orbit in Kerbal Space Program.
+#Created by Eric Fernald.
+
+"""
+To run this KSP kRPC Python program you need:
+- Kerbal Space Program (tested in 1.5.1)
+- OrbitRocket Craft file included in the directory.
+- kRPC 0.4.8 (https://github.com/krpc/krpc/releases/download/v0.4.8/krpc-0.4.8.zip)
+  Use the install guide on https://krpc.github.io/krpc/getting-started.html
+- I've also installed the Python client library (https://pypi.python.org/pypi/krpc)
+- Python 3.9.1 (https://www.python.org/downloads/release/python-391/)
+"""
+
 import krpc
+import time
 
-TARGET_APOAPSIS = 690000
-TARGET_PERIAPSIS = 690000
-CRUISE_ALTITUDE = 80000
-GRAVITY_TURN_ALTITUDE = 50000
-LAUNCH_HEADING = 90
-COUNTDOWN_WORDS = ("Five", "Four", "Three", "Two", "One", "LIFT OFF!")
+#Establishes connection to the krpc server.
+conn = krpc.connect()
 
-def clamp(value, lower, upper):
-    return max(lower, min(upper, value))
+#Establishes the rocket as the main vessel.
+vessel = conn.space_center.active_vessel
 
-class RocketLaunchController:
-    def __init__(self, connection, vessel):
-        self.conn = connection
-        self.vessel = vessel
-        flight = self.vessel.flight()
-        orbit = self.vessel.orbit
-        self._altitude = self.conn.add_stream(getattr, flight, "mean_altitude")
-        self._pitch = self.conn.add_stream(getattr, flight, "pitch")
-        self._heading = self.conn.add_stream(getattr, flight, "heading")
-        self._apoapsis = self.conn.add_stream(getattr, orbit, "apoapsis")
-        self._periapsis = self.conn.add_stream(getattr, orbit, "periapsis")
-        self._thrust = self.conn.add_stream(getattr, self.vessel, "thrust")
-        self.auto_pilot = self.vessel.auto_pilot
-        self.auto_pilot.reference_frame = self.vessel.surface_reference_frame
-        self.auto_pilot.stopping_time = (0.5, 0.5, 0.5)
+#Countdown Sequence.
+countdown = ["Five", "Four", "Three", "Two", "One", "LIFT OFF!"]
 
-    def run(self):
-        self.perform_countdown()
-        self.prepare_launch()
-        self.handle_ascent()
-        self.coast_to_space()
-        self.perform_insertion()
-        self.wrap_up()
+#Start the Countdown Sequence.
+for i in range(len(countdown)):
+    print(countdown[i])
+    time.sleep(1)
 
-    def perform_countdown(self):
-        for word in COUNTDOWN_WORDS:
-            print(word)
-            time.sleep(1)
+vessel.control.throttle = 1
+vessel.control.activate_next_stage()
 
-    def prepare_launch(self):
-        self.vessel.control.throttle = 1
-        self.auto_pilot.target_pitch_and_heading(90, LAUNCH_HEADING)
-        self.auto_pilot.engage()
-        self.vessel.control.activate_next_stage()
+#Flight State.
+ascentPhase = True
+cruisePhase = False
+insertionPhase = False
 
-    def handle_ascent(self):
-        while True:
-            altitude = self._altitude()
-            pitch = self.gravity_turn_pitch(altitude)
-            self.auto_pilot.target_pitch_and_heading(pitch, LAUNCH_HEADING)
-            self.stage_if_needed()
-            if self._apoapsis() >= TARGET_APOAPSIS:
-                self.vessel.control.throttle = 0
-                self.auto_pilot.disengage()
-                time.sleep(0.5)
-                self.stage_if_needed(force=True)
-                self.vessel.control.sas = True
-                time.sleep(0.1)
-                self.vessel.control.sas_mode = self.conn.space_center.SASMode.prograde
-                break
-            time.sleep(0.1)
+#Main Launch Control.
+while ascentPhase or cruisePhase or insertionPhase:
+    altitude = vessel.flight().mean_altitude
+    heading = vessel.flight().heading
 
-    def coast_to_space(self):
-        while self._altitude() <= CRUISE_ALTITUDE:
+    #Ascent Phase.
+    if ascentPhase:
+        targetPitch = 90 * ((50000 - altitude) / 50000)
+        pitchDiff = vessel.flight().pitch - targetPitch
+
+        #Heading Control.
+        if heading < 180:
+            vessel.control.yaw = (pitchDiff / 90)
+        else:
+            vessel.control.yaw = 0.5
+
+        #Adds another stage activation to the vessel if more than one stage is required to get to orbit.
+        if vessel.thrust == 0.0:
+            vessel.control.activate_next_stage()
+        
+        #MECO (Main Engine Cut Off)
+        if vessel.orbit.apoapsis > 690000:
+            vessel.control.throttle = 0
             time.sleep(0.5)
-        self.vessel.control.sas = False
-        self.vessel.control.throttle = 1
-        self.auto_pilot.engage()
-        self.auto_pilot.target_pitch_and_heading(0, LAUNCH_HEADING)
+            vessel.control.activate_next_stage()
 
-    def perform_insertion(self):
-        while True:
-            pitch_error = self._pitch()
-            if abs(pitch_error) <= 1:
-                self.vessel.control.sas = True
-                self.vessel.control.sas_mode = self.conn.space_center.SASMode.prograde
-                self.auto_pilot.disengage()
-            else:
-                if not self.auto_pilot.engaged:
-                    self.vessel.control.sas = False
-                    self.auto_pilot.engage()
-                self.auto_pilot.target_pitch_and_heading(0, LAUNCH_HEADING)
-            self.stage_if_needed()
-            if self._periapsis() >= TARGET_PERIAPSIS:
-                self.vessel.control.throttle = 0
-                break
+            vessel.control.sas = True
             time.sleep(0.1)
+            vessel.control.sas_mode = conn.space_center.SASMode.prograde
 
-    def wrap_up(self):
-        if self.auto_pilot.engaged:
-            self.auto_pilot.disengage()
-        self.vessel.control.sas = True
-        self.vessel.control.sas_mode = self.conn.space_center.SASMode.prograde
+            ascentPhase = False
+            cruisePhase = True
+    
+    #Cruise Phase.
+    elif cruisePhase:
+        if altitude > 80000:
+            cruisePhase = False
+            insertionPhase = True
+            vessel.control.sas = False
+            vessel.control.throttle = 1
 
-    def gravity_turn_pitch(self, altitude):
-        if altitude >= GRAVITY_TURN_ALTITUDE:
-            return 0
-        ratio = (GRAVITY_TURN_ALTITUDE - altitude) / GRAVITY_TURN_ALTITUDE
-        return clamp(90 * ratio, 0, 90)
+    #Insertion Phase.
+    elif insertionPhase:
+        targetPitch = 0
+        pitchDiff = vessel.flight().pitch - targetPitch
 
-    def stage_if_needed(self, force=False):
-        if not force and (self.vessel.control.throttle <= 0 or self._thrust() > 0.1):
-            return
-        if self.vessel.control.current_stage > 0:
-            self.vessel.control.activate_next_stage()
+        #Heading Control.
+        if heading < 180:
+            vessel.control.yaw = (pitchDiff / 90)
+            if vessel.flight().pitch < 1 and vessel.flight().pitch > -1:
+                vessel.control.sas = True
+            else:
+                vessel.control.sas = False
+        else:
+            vessel.control.yaw = 0.5
 
-def main():
-    connection = krpc.connect(name="RocketLaunch")
-    vessel = connection.space_center.active_vessel
-    RocketLaunchController(connection, vessel).run()
-
-if __name__ == "__main__":
-    main()
+        #SECO (Second Engine Cut Off).
+        if vessel.orbit.periapsis > 690000:
+            vessel.control.throttle = 0
+            insertionPhase = False
+        
+        #Staging.
+        if vessel.thrust == 0.0:
+            vessel.control.activate_next_stage()
