61d3f8d5807b135447a79d41cf2aa0308eacd61e
Eric-Fernald <ejf4595@gmail.com>
2026-05-01 18:08:32 -0400

    Refactor fetch.go to replace ioutil.ReadAll with io.ReadAll and improve comments

--- Projects/Go_Projects/fetch/fetch.go
+++ Projects/Go_Projects/fetch/fetch.go
@@ -3,20 +3,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
+	// Iterate over each URL provided as a command-line argument.
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
 		if err != nil {
+			// Write error to stderr so it's separable from normal output, then exit.
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close() // must close to avoid leaking the underlying TCP connection
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
